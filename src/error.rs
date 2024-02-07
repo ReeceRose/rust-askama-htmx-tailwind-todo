4a792e83c72ac3c9408b8f6b210750b203c84bc5
Reece Rose <reecerose18@gmail.com>
2024-02-06 22:16:37 -0500

    Bump tokio. Code cleanup

--- src/error.rs
+++ src/error.rs
@@ -1,34 +1,4 @@
-use axum::{
-    http::StatusCode,
-    response::{IntoResponse, Response},
-};
 use thiserror::Error;
-
-// TODO: refactor to remove error?
-// Make our own error that wraps `anyhow::Error`.
-pub struct AppError(anyhow::Error);
-
-// Tell axum how to convert `AppError` into a response.
-impl IntoResponse for AppError {
-    fn into_response(self) -> Response {
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Something went wrong: {}", self.0),
-        )
-            .into_response()
-    }
-}
-
-// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
-// `Result<_, AppError>`. That way you don't need to do that manually.
-impl<E> From<E> for AppError
-where
-    E: Into<anyhow::Error>,
-{
-    fn from(err: E) -> Self {
-        Self(err.into())
-    }
-}
 
 #[derive(Error, Debug)]
 pub enum TodoError {
@@ -45,27 +15,3 @@ pub enum TodoError {
     #[error("Failed to delete todo. Please try again.")]
     FailedToDelete,
 }
-
-// impl IntoResponse for TodoError {
-//     fn into_response(self) -> Response {
-//         let mut target = "this";
-//         match self {
-//             Self::FailedToCreate => {}
-//             Self::FailedToDelete => {}
-//             Self::FailedToUpdate => {}
-//             Self::FailedToGet => target = "#load-error",
-//             Self::FailedToGetLock => {}
-//             Self::NotFound => {}
-//         }
-//         (
-//             StatusCode::OK,
-//             [
-//                 ("HX-Retarget", target),
-//                 ("HX-Reswap", "innerHTML"),
-//                 ("content-type", "text/html"),
-//             ],
-//             format!("Something went wrong: {}", self),
-//         )
-//             .into_response()
-//     }
-// }
--- src/routes/htmx/index.rs
+++ src/routes/htmx/index.rs
@@ -1,12 +1,11 @@
-use crate::{error::AppError, service::todo::TodoServiceImpl, templates::index::GetIndexResponse};
+use crate::{service::todo::TodoServiceImpl, templates::index::GetIndexResponse};
 
-use anyhow::Result;
 use axum::routing::{delete, get, Router};
 
 use super::todo::{delete_todo, get_todos, post_todo, toggle_todo};
 
-pub async fn get_index() -> Result<GetIndexResponse, AppError> {
-    Ok(GetIndexResponse)
+pub async fn get_index() -> GetIndexResponse {
+    GetIndexResponse {}
 }
 
 pub fn create_htmx_routes() -> Router<TodoServiceImpl> {
